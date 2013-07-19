4a79fbfb0e836c8fcd883239d8b6f9f5cc1124ba
Binish A.R <arbinish@gmail.com>
2013-07-20 02:08:03 +0530

    notifier.py: client for irc.py

---
+++ notifier.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+''' Client/Subscriber part of IRC remote session notifier '''
+import zmq
+import time
+import subprocess
+
+''' specifiy the ZMQ/Publisher port '''
+PORT = "5892"
+''' Specify the remote host where your irc session is running
+	technically it can be any remote node which can connect to irc and 
+	which has necessary acl to communicate with subscriber '''
+HOST = '10.14.116.32'
+
+ctx = zmq.Context()
+sock = ctx.socket(zmq.SUB)
+sock.connect("tcp://%s:%s" % (HOST, PORT))
+''' we know that the publisher publishes messages to "notify" channel '''
+sock.setsockopt(zmq.SUBSCRIBE, 'notify')
+
+while True:
+	_ret = sock.recv()
+	topic, messagedata = _ret.split(None, 1)
+	''' I am using mac os x. Linux users may replace terminal-notifier with notify-send with appropriate args '''
+	notifier_args = [
+	  '/usr/bin/terminal-notifier',
+	  '-title',
+	  topic,
+	  '-message',
+	  '%r' % messagedata,
+	  '-activate',
+	  'com.apple.Terminal',
+	]
+#	print 'Topic: %s messagedata: %s' % (topic, messagedata)
+	subprocess.check_call(notifier_args)
+	
+
+sock.close()
